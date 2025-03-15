9f05d4351e50a5eb5953631df662ca2452236520
Monem Ahmed <monem.ahmed2k15@gmail.com>
2025-03-15 12:36:00 +0600

    changes are as follows: 1. Added Employee class 2. Added test code for Employee class

---
+++ Employee.cpp
@@ -0,0 +1,69 @@
+#include <iostream>
+#include "Employee.h"
+namespace Records {
+	Employee::Employee(const std::string& firstName,
+		const std::string& lastName)
+		:m_firstName{ firstName }, m_lastName{ lastName }
+	{
+	}
+	void Employee::promote(int raiseAmount)
+	{
+		setSalary(getSalary() + raiseAmount);
+	}
+	void Employee::demote(int demeritAmount)
+	{
+		setSalary(getSalary() - demeritAmount);
+	}
+	void Employee::hire()
+	{
+		m_hired = true;
+	}
+	void Employee::fire()
+	{
+		m_hired = false;
+	}
+	void Employee::display() const
+	{
+		std::cout << "Employee Information" << std::endl;
+		std::cout << "Name: " << getFirstName() << " " << getLastName() << std::endl;
+		std::cout << "Employment: " << (isHired() ? "Current Employee" : "Former Employee") << std::endl;
+		std::cout << "ID: " << getEmployeeNumber() << std::endl;
+		std::cout << "Remuneration: " << getSalary() << std::endl;
+	}
+	void Employee::setFirstName(const std::string& firstName)
+	{
+		m_firstName = firstName;
+	}
+	const std::string& Employee::getFirstName() const
+	{
+		return m_firstName;
+	}
+	void Employee::setLastName(const std::string& lastName)
+	{
+		m_lastName = lastName;
+	}
+	const std::string& Employee::getLastName() const
+	{
+		return m_lastName;
+	}
+	void Employee::setEmployeeNumber(int employeeNumber)
+	{
+		m_employeeNumber = employeeNumber;
+	}
+	int Employee::getEmployeeNumber() const
+	{
+		return m_employeeNumber;
+	}
+	void Employee::setSalary(int salary)
+	{
+		m_salary = salary;
+	}
+	int Employee::getSalary() const
+	{
+		return m_salary;
+	}
+	bool Employee::isHired() const
+	{
+		return m_hired;
+	}
+}
